ee361af0a65ab09ae6ee4807947fd302416791c5
Mark Ferraretto <52906017+LLAW3301@users.noreply.github.com>
2023-05-08 11:51:32 +0930

    Create prototype branch and add word doc

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.LLAW33012023S1LDO1',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.LLAW33012023S1LDO1\n\nA docassemble extension.\n\n## Author\n\nIsabella Trigwell, trig0040@flinders.edu.au\n\n',
+      long_description='# docassemble.LLAW33012023S1LDO1\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nIsabella Trigwell, trig0040@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Isabella Trigwell',
       author_email='trig0040@flinders.edu.au',
